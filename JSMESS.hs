08fde26793961e99aeaaec8676e70c4b6a15b035
Daniel Brooks <db48x@db48x.net>
2017-03-12 20:51:30 -0700

    set up emsdk/dosbox/mame source

--- JSMESS.hs
+++ JSMESS.hs
@@ -1,5 +1,7 @@
 module JSMESS where
 
+import Utility.FileMode
+
 admin :: User -> Ssh.PubKeyText -> Property DebianLike
 admin u@(User n) k = propertyList ("admin user " ++ n) $ props
 	& User.accountFor u
@@ -7,3 +9,12 @@ admin u@(User n) k = propertyList ("admin user " ++ n) $ props
 	& Sudo.enabledFor u
 	& Ssh.authorizedKey u k
 	& Ssh.authorizedKey (User "root") k
+
+staffOwnedGid :: FileMode.FilePath -> Property DebianLike
+staffOwnedGid path = File.dirExists path
+    & File.ownerGroup path (User "root") (Group "staff")
+    & File.mode path (combineModes (readModes ++ executeModes ++
+                                    [ ownerWriteMode
+                                    , groupWriteMode
+                                    , setGroupIDMode
+                                    ]))
--- config.hs
+++ config.hs
@@ -29,3 +29,17 @@ buildmachine = host "buildmachine.archiveteam.org" $ props
 --   & JSMESS.admin (User "sketchcow") ""
 --   & JSMESS.admin (User "bai") ""
 --   & JSMESS.admin (User "vito") ""
+    & Apt.buildDep [ "mame" "dosbox" ]
+    & JSMESS.staffOwned srcdir </> "emsdk"
+    & cmdProperty "wget" [ "https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz"
+                         , "-O", emsdktar]
+    & cmdProperty "tar" ["xf", emsdktar
+                        , "-C", srcdir </> "emsdk"
+                        , "--strip-components=1"]
+    & cmdProperty (srcdir </> "emsdk" </> "emsdk") [ "install", "sdk-incoming-64bit" ]
+    & JSMESS.staffOwned (srcdir </> "dosbox")
+    & Git.cloned (User "db48x") "https://github.com/dreamlayers/em-dosbox/" (srcdir </> "dosbox") (Just "master")
+    & JSMESS.staffOwned (srcdir </> "mame")
+    & Git.cloned (User "db48x") "https://github.com/mamedev/mame" (srcdir </> "mame") (Just "master")
+  where srcdir = "/usr/local/src"
+        emsdktar = srcdir </> "emsdk-portable-tar.gz"
