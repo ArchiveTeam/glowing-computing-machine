00a92d644821074064e0c66f639075e4a298e83b
Daniel Brooks <db48x@db48x.net>
2017-03-14 22:07:54 -0700

    misc obvious changes

--- JSMESS.hs
+++ JSMESS.hs
@@ -1,17 +1,24 @@
 module JSMESS where
 
+import Propellor
+import System.Posix.Files
 import Utility.FileMode
+import qualified Propellor.Property.File as File
+import qualified Propellor.Property.Ssh as Ssh
+import qualified Propellor.Property.User as User
+import qualified Propellor.Property.Sudo as Sudo
 
 admin :: User -> Ssh.PubKeyText -> Property DebianLike
 admin u@(User n) k = propertyList ("admin user " ++ n) $ props
-	& User.accountFor u
-	& User.hasGroup u (Group "staff")
-	& Sudo.enabledFor u
-	& Ssh.authorizedKey u k
-	& Ssh.authorizedKey (User "root") k
+    & User.accountFor u
+    & User.hasGroup u (Group "staff")
+    & Sudo.enabledFor u
+    & Ssh.authorizedKey u k
+    & Ssh.authorizedKey (User "root") k
 
-staffOwnedGid :: FileMode.FilePath -> Property DebianLike
-staffOwnedGid path = File.dirExists path
+staffOwned :: FilePath -> Property UnixLike
+staffOwned path = propertyList ("path "++ path ++" is owned by staff") $ props
+    & File.dirExists path
     & File.ownerGroup path (User "root") (Group "staff")
     & File.mode path (combineModes (readModes ++ executeModes ++
                                     [ ownerWriteMode
--- config.hs
+++ config.hs
@@ -2,11 +2,13 @@
 -- the propellor program.
 
 import Propellor
-import qualified Propellor.Property.File as File
 import qualified Propellor.Property.Apt as Apt
 import qualified Propellor.Property.Cron as Cron
 import qualified Propellor.Property.User as User
+import qualified Propellor.Property.Git as Git
 import qualified JSMESS
+import System.Directory
+import System.FilePath.Posix
 
 main :: IO ()
 main = defaultMain hosts
@@ -26,20 +28,25 @@ buildmachine = host "buildmachine.archiveteam.org" $ props
     & User.hasSomePassword (User "root")
     & Cron.runPropellor (Cron.Times "30 * * * *")
     & JSMESS.admin (User "db48x") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAMFoRm8trenxhZWe6dDEB2c6POPbsPfM5ArZep9lU+ db48x@anglachel"
---   & JSMESS.admin (User "sketchcow") ""
---   & JSMESS.admin (User "bai") ""
---   & JSMESS.admin (User "vito") ""
-    & Apt.buildDep [ "mame" "dosbox" ]
-    & JSMESS.staffOwned srcdir </> "emsdk"
-    & cmdProperty "wget" [ "https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz"
-                         , "-O", emsdktar]
-    & cmdProperty "tar" ["xf", emsdktar
-                        , "-C", srcdir </> "emsdk"
-                        , "--strip-components=1"]
-    & cmdProperty (srcdir </> "emsdk" </> "emsdk") [ "install", "sdk-incoming-64bit" ]
+    & JSMESS.admin (User "db48x") "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQkqIgZ7D8WHW5Y3o+fpZC/4xtv/3IQrORJrTPCt7KY db48x@erebor"
+--   & JSMESS.admin (User "sketchcow") [""]
+--   & JSMESS.admin (User "bai") [""]
+--   & JSMESS.admin (User "vito") [""]
+    & Apt.buildDep [ "mame", "dosbox" ]
+    & JSMESS.staffOwned (srcdir </> "emsdk")
+    & check (not <$> doesFileExist emsdktar)
+      (cmdProperty "wget" [ "https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz"
+                          , "-O", emsdktar])
+    & check (not <$> doesFileExist emsdk)
+      (cmdProperty "tar" ["xf", emsdktar
+                          , "-C", srcdir </> "emsdk"
+                          , "--strip-components=1"])
+    & cmdProperty emsdk [ "update" ] `assume` MadeChange
+    & cmdProperty emsdk [ "install", "sdk-incoming-64bit" ] `assume` MadeChange
     & JSMESS.staffOwned (srcdir </> "dosbox")
     & Git.cloned (User "db48x") "https://github.com/dreamlayers/em-dosbox/" (srcdir </> "dosbox") (Just "master")
     & JSMESS.staffOwned (srcdir </> "mame")
     & Git.cloned (User "db48x") "https://github.com/mamedev/mame" (srcdir </> "mame") (Just "master")
   where srcdir = "/usr/local/src"
         emsdktar = srcdir </> "emsdk-portable-tar.gz"
+        emsdk = (joinPath [ srcdir, "emsdk", "emsdk" ])
