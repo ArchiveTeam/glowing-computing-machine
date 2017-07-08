0ae913f8f833cde92b5f4b73aaf769e08b4034e9
Daniel Brooks <db48x@db48x.net>
2017-07-07 20:13:53 -0700

    propellor spin

--- config.hs
+++ config.hs
@@ -37,9 +37,9 @@ buildmachine = host "glowing-computing-machine.db48x.net" $ props
     & JSMESS.defaultUmask 0o002
     & JSMESS.admin (User "db48x") [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAMFoRm8trenxhZWe6dDEB2c6POPbsPfM5ArZep9lU+ db48x@anglachel"
                                   , "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJQkqIgZ7D8WHW5Y3o+fpZC/4xtv/3IQrORJrTPCt7KY db48x@erebor" ]
---   & JSMESS.admin (User "sketchcow") [""]
---   & JSMESS.admin (User "bai") [""]
---   & JSMESS.admin (User "vito") [""]
+    & JSMESS.admin (User "sketchcow") [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD7+Q8vqXSA5M0meUhTu0VoYAmaPXu3MeKzMyxS9OsQOMb0ZvyBAIHEWaUu96jiL047qZzy2Fq3ahAqbmzriSeV6eMfJEDRU4d6H2pVWD56pEssm/eW51MQJePI0NkdnZFXEr0KWGuwCwLQ76Ef18sDl5mECy4f8BHZYaGs3FIzuHhqOilrVByRz+rnr2+3uHI2qvkVMCV8Fwx5qoWP2EzN5cTiruyXDiXD/uu/uHZn0fnSOJ3YXcoCs9GVPlhlbtB2cR7RprioHqFC01u/GzG9BoBOykRZP1sp1LB+U5DeACw45Szk1xvgdyrXk9HFCkvSi+IPZb64XmQBVQQGtoGOoD3AGS3noAD0NFULQwegB9FuNFoOIZbyBMfSInM9JZrcqPbn6YFvM+RMj5JQ0pkdMR5OH7C4UtTxbwErtwcC5isD9zQMn/caGO9ULnz+xsgVYAOMwDMM1YXY8EGA7A/sl4l1GQ1ENVhwlUBFRL7eOevZiqV4C58DnwXG5FLN7tRBxe9SMISIWtxsarJQV1rWHqjLG9BDvJlugaFubJvZ9X1TI3JVofa1l0u63wHOImdVst1s6NxGh91xoiDnsie9SIaav6WdVNVqmb/PszuCkHPR8Ljc3WBURRT3osxFtCSN4BpiMpY2G75c5+OzFbjPlztAIfvpZbi5LBXy9pnjnQ== root@teamarchive0.fnf.archive.org" ]
+--    & JSMESS.admin (User "bai") [ "" ]
+--    & JSMESS.admin (User "vito") [""]
     & Apt.installed [ "vim", "emacs-nox", "nano" ] -- gotta have the right editor
     & Apt.installed [ "mosh", "tmux", "screen" ]
     & Apt.buildDep [ "dosbox" ] -- we also want this for mame, but mame has no src package?
